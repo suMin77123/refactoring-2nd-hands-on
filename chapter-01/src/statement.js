e315d8357eed15e0d8c3eba5828dd74d7b9163c3
suMin <sumin77123@gmail.com>
2025-07-20 23:21:50 +0900

    refactor: 15-변수 인라인하기

--- chapter-01/src/statement.js
+++ chapter-01/src/statement.js
@@ -10,10 +10,8 @@ export default function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
   function amountFor(aPerformance) {
