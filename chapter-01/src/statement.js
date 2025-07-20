7b5e63590811bca15e63a73b8d50f1f93f81efce
suMin <sumin77123@gmail.com>
2025-07-20 23:18:57 +0900

    refactor: 14-함수 추출하기

--- chapter-01/src/statement.js
+++ chapter-01/src/statement.js
@@ -10,10 +10,7 @@ export default function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
@@ -60,5 +57,13 @@ export default function statement(invoice, plays) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
 }
