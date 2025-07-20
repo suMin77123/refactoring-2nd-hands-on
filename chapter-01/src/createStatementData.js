7930857865dda662eb94ac23917fcf7c7fefcb2c
suMin <sumin77123@gmail.com>
2025-07-21 01:25:28 +0900

    refactor: 34-함수 선언 바꾸기

--- chapter-01/src/createStatementData.js
+++ chapter-01/src/createStatementData.js
@@ -7,9 +7,12 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -60,7 +63,8 @@ export default function createStatementData(invoice, plays) {
 }
 
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
